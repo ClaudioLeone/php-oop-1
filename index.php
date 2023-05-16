f20dd251320905ac9a58dd08ce1b10462ebb7ff4
ClaudioLeone <claleone92@gmail.com>
2023-05-16 17:02:10 +0200

    Created 'Movie' class [contructor, 2 methods]

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php
-    
+    class Movie {
+        public $movTitle;
+        public $movDescription;
+        public $movRating;
+        public $movLanguage;
+
+        function __construct($_movTitle) {
+            $this->movTitle = $_movTitle;
+        }
+
+        public function setMovData($_movRating, $_movLanguage, $_movDescription) {
+            $this->movDescription = $_movDescription;
+            $this->movRating = $_movRating;
+            $this->movLanguage = $_movLanguage;
+        }
+
+        public function printMov() {
+            return "{$this->movTitle} {$this->movDescription} {$this->movRating} {$this->movLanguage}";
+        }
+    }
 ?>
 
 <!DOCTYPE html>
